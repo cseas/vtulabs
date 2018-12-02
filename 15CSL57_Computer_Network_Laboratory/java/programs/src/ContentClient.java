b395a1ab9d61e376d046e245ac5bc628a757eadf
cseas <contact.abhijeetsingh@gmail.com>
2018-12-02 14:58:45 +0530

    Add content client server

--- 15CSL57_Computer_Network_Laboratory/java/programs/src/ContentClient.java
+++ 15CSL57_Computer_Network_Laboratory/java/programs/src/ContentClient.java
@@ -3,5 +3,33 @@
    contents of the requested file if present.
    Created by Abhijeet Singh
  */
+import java.io.*;
+import java.net.*;
 public class ContentClient {
+    public static void main(String[] args) throws Exception {
+        Socket sock = new Socket("127.0.0.1", 4000);
+
+        InputStreamReader isr = new InputStreamReader(System.in);
+        BufferedReader br = new BufferedReader(isr);
+
+        System.out.println("Enter file name:");
+        String fname = br.readLine();
+
+        OutputStream ostream = sock.getOutputStream();
+        PrintWriter pwrite = new PrintWriter(ostream, true);
+        pwrite.println(fname);
+
+        InputStream istream = sock.getInputStream();
+        BufferedReader socketRead = new BufferedReader(new InputStreamReader(istream));
+        String str;
+
+        while((str = socketRead.readLine()) != null) {
+            System.out.println(str);
+        }
+
+        sock.close();
+        br.close();
+        socketRead.close();
+        pwrite.close();
+    }
 }
